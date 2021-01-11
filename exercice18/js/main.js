cee922cbe28031846b64031caa903fd264ef8cb3
atif <at07.if@gmail.com>
2021-01-11 20:49:08 +0100

    exercice18 terminé

---
+++ exercice18/js/main.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#text").hide("Konexio!")
+        // $("#text").css("display", "non")
+        // la méthode hide correspond la régle CSS ==> ("display", "non")
+    })
+})
