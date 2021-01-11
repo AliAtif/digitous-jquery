7c3190718751769612a4b31ee7537aaa73ea05aa
atif <at07.if@gmail.com>
2021-01-11 20:19:56 +0100

    exercice17 terminé

---
+++ exercice17/js/main.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#text").show("Konexio!")
+        // $("#text").css("display", "block")
+        // la méthode show  correspond la régle CSS ==> ("display", "block")
+    })
+})
