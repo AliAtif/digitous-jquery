e322806e15c4605b084f2a2700ba85b6519455ab
atif <at07.if@gmail.com>
2021-01-13 14:25:02 +0100

    exercice32 terminé

--- exercice32/js/main.js
+++ exercice32/js/main.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
             url:"https://restcountries.eu/rest/v2/name/france",
             success: function(data, status, response){
                 console.log(response.statusCode);
-                $("#france").text(data.map((function(france){
+                $("#exercise").text(data.map((function(france){
                 
                     console.log(france.name)
                 })));
               
-                $("#france").text(data.map((function(france){
+                $("#exercise").text(data.map((function(france){
                     console.log(france.capital)
                 })));
             }
