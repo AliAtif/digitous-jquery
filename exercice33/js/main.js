ceb7dcadf1d30fde5f41e54a6915e298dc10fe63
atif <at07.if@gmail.com>
2021-01-13 17:02:03 +0100

    exercice33 terminé

--- exercice33/js/main.js
+++ exercice33/js/main.js
@@ -4,39 +4,42 @@
 
 $(document).ready(function(){
     $(".btn").click(function(){
-    //    $.ajax({
-    //         url:"https://restcountries.eu/rest/v2/name/france",
-    //         success: function(data, status, response){
-
-    //             console.log(response.statusCode);
-
-    //             $("#country").val(data.map((function(france){
-                  
-    //                $("#country").html(france.name);
-              
-    //             })));
-              
-    //             $("#capital").val(data.map((function(france){
-
-    //                 $("#capital").html(france.capital);
-    //             })));
-    //         }
-    //     })
-
-    })
-
-    $("button").click(function(){
         $.ajax({
-            url: 'https://restcountries.eu/rest/v2/name/france',
-            dataType: 'json',
-            success: function(data) {
-                $country=data[0].name
-                $capital=data[0].capital
-             $("#country").html($country)
-             $("#capital").html($capital)
-            },
-       
-        });
+            url:"https://restcountries.eu/rest/v2/name/france",
+            success: function(data, status, response){
+
+                 console.log(response.statusCode);
+
+                 $country=data[0].name
+                 $capital=data[0].capital
+
+                 $("#country").val(data.map((function(data){
+                    $("#country").html($country);
+              
+                 })));
+              
+                 $("#capital").val(data.map((function(data){
+                     $("#capital").html( $capital);
+                })));
+             }
+         })
+
     })
+
+
+
+    // $("button").click(function(){
+    //     $.ajax({
+    //         url: 'https://restcountries.eu/rest/v2/name/france',
+    //         dataType: 'json',
+    //         success: function(data) {
+    //             $country=data[0].name
+    //             $capital=data[0].capital
+    //          $("#country").html($country)
+    //          $("#capital").html($capital)
+    //         },
+       
+    //     });
+    // })
    
 })
