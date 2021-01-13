46d32798409601d21ea509e02d9abdd28b8e5141
atif <at07.if@gmail.com>
2021-01-13 12:19:49 +0100

    exercice30 terminé

--- exercice30/js/main.js
+++ exercice30/js/main.js
@@ -5,11 +5,12 @@
 $(document).ready(function(){
     $(".form-control").keyup(function(){
 
-        if($(".form-control").val().length>5){
-            $(".form-control").addClass("is-valid")
+        if($(".form-control").val().length < 5){
+            $(".form-control").addClass("is-invalid")
            
         }else{
-            $(".form-control").addClass("is-invalid");
+            $(".form-control").removeClass("is-invalid");
+            $(".form-control").addClass("is-valid");
          
         }
        
