aaf35651e21416fa5432c65e3d90a1b30524249e
atif <at07.if@gmail.com>
2021-01-14 17:41:19 +0100

    sudoku

---
+++ exercice34/js/main.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $(".form-control").val()
+    });
+})
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".btn").click(function(){
+       $.ajax({
+            url:"https://restcountries.eu/rest/v2/name/france",
+            success: function(data, status, response){
+                console.log(response.statusCode);
+
+                $("#country").val(data.map((function(france){
+                  
+                   $("#country").html(france.name);
+              
+                })));
+              
+                $("#capital").val(data.map((function(france){
+                    $("#capital").html(france.capital);
+                  
+                
+                })));
+            }
+
+        })
+    })
+   
+})
